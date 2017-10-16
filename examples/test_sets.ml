60c63443568d214d16c6866cbfcfd684567231c0
Mindy Preston <meetup@yomimono.org>
2017-10-16 13:09:06 -0500

    hello world

---
+++ examples/test_sets.ml
@@ -0,0 +1,19 @@
+module Set_tester(Elt: Set.OrderedType) (G: Shims.GENERABLE with type t = Elt.t)
+= struct
+  module Set = Set.Make(Elt)
+
+  let set = Crowbar.(Choose [
+
+    ])
+
+  let noddy = fun _ -> Crowbar.check true
+
+  let add_tests () =
+    Crowbar.add_test Crowbar.[set] noddy
+
+end
+
+module UcharTester = Set_tester(Uchar)(Shims.Uchar)
+
+let () =
+  UcharTester.add_tests ();
