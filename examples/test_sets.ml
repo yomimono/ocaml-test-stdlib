3eb8c983d26d9fef228ca3fda9c36276ab75a752
Mindy Preston <meetup@yomimono.org>
2017-10-16 13:24:47 -0500

    put an actual test in there

--- examples/test_sets.ml
+++ examples/test_sets.ml
@@ -6,10 +6,17 @@ module Set_tester(Elt: Set.OrderedType) (G: Shims.GENERABLE with type t = Elt.t)
       Map ([List G.gen], Set.of_list);
     ])
 
-  let noddy = fun _ -> Crowbar.check true
+  let check_min_max s =
+    match Set.(min_elt s, max_elt s) with
+    | min, max when Elt.compare min max = 0 ->
+      Crowbar.check_eq 1 @@ Set.cardinal s
+    | min, max -> Elt.compare max min > 0 |> Crowbar.check
+    | exception Not_found -> Crowbar.check @@ Set.is_empty s
+
 
   let add_tests () =
-    Crowbar.add_test Crowbar.[set] noddy
+    Crowbar.add_test ~name: "Set.min >= Set.max only when the set has 1 element"
+      Crowbar.[set] check_min_max
 
 end
 
