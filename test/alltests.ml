e6e6858e8bd9deef268784d814c6cd9e586c5aa5
Mindy <meetup@yomimono.org>
2018-07-03 16:45:33 -0500

    alltests no longer makes sense

--- test/alltests.ml
+++
@@ -1,9 +0,0 @@
-let () =
-  Stdlibtest.Tests.add_basetests ();
-  Stdlibtest_406.Maps.(
-    StringTester.add_tests ();
-    IntStringTester.add_tests ();
-    CharIntTester.add_tests ();
-    NativeIntTester.add_tests ();
-    UcharStringTester.add_tests ();
-  )
